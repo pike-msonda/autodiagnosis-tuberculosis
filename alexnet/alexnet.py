f2225e88d170ef9e680e7eb671ef5acf0c8b0b84
RenoGunray <pike.msonda@gmail.com>
2019-06-20 11:53:35 +0300

    fixing up alexnet model

--- alexnet/alexnet.py
+++ alexnet/alexnet.py
@@ -18,14 +18,15 @@ class AlexNet:
         self.classes = classes
 
     def conv_layer(self, x, filters,kernel_size, padding= "same", 
-            kernel_regularizer=l2(0), strides = (1,1),
-                 max_pooling=True, activation="relu", name=None): 
+            kernel_regularizer=l2(0), strides=(1,1), max_pooling=True, 
+            activation="relu", name=None): 
 
-        x = Conv2D(filters, kernel_size ,strides =strides,padding=padding, 
+        x = Conv2D(filters, kernel_size, strides=strides, padding=padding, 
             activation=activation)(x)
-        if (max_pooling == True):
+        if (max_pooling):
             x = MaxPooling2D(pool_size=(3,3), strides=(2,2))(x)
-        x = BatchNormalization()(x)
+
+        x = BatchNormalization()(x) # apply batch normalisation. 
         return x
 
     def output_layer(self,x, classes):
@@ -43,57 +44,42 @@ class AlexNet:
     def model(self):    
         # 1st LAYER
         x =  self.conv_layer(self.init, filters=96, kernel_size=(11,11), strides=(4,4),
-        padding="valid", max_pooling=True, activation='relu', name='conv_1')
+            padding="valid", max_pooling=True, activation='relu', name='conv_1')
 
-        #2nd Layer
-        x =  self.conv_layer(x, filters=256, kernel_size=(5,5), strides=(1,1),
-            padding="valid", max_pooling=True, name="conv_2")
+        # 2nd Layer
+        x =  self.conv_layer(x, filters=256, kernel_size=(5,5),strides=(1,1),
+            padding="same", max_pooling=True, name="conv_2")
 
         # 3RD LAYER
-        x = ZeroPadding2D((1,1))(x)
-
-        x =  self.conv_layer(x, filters=384, kernel_size=(3,3), strides=(1,1),padding="valid",
-        max_pooling=False, name="conv_3")
+        x =  self.conv_layer(x, filters=384, kernel_size=(3,3),strides=(1,1),
+            padding="same",max_pooling=False, name="conv_3")
         
-        x = ZeroPadding2D((1,1))(x)
 
         # 4Th LAYER
-        x =  self.conv_layer(x, filters=384, kernel_size=(3,3), strides=(1,1), 
-        padding="valid", max_pooling=False, name="conv_4")
-
-        x = ZeroPadding2D((1,1))(x)
+        x =  self.conv_layer(x, filters=384, kernel_size=(3,3),strides=(1,1), 
+            padding="same", max_pooling=False, name="conv_4")
 
         # 5Th LAYER
-        x =  self.conv_layer(x, filters=256, kernel_size=(3,3), strides=(1,1),
-        padding="valid", max_pooling=True, name="conv_5")
+        x =  self.conv_layer(x, filters=256, kernel_size=(3,3),strides=(1,1),
+            padding="same", max_pooling=True, name="conv_5")
 
         # 6 FLATTEN 
         x = Flatten()(x)
-        
+
+        x = Dropout(0.5)(x)
+
         # Fully Connected LAYER 1
         x = Dense(4096)(x)
         x = Activation('relu')(x)
         x = Dropout(0.5)(x)
 
-        #FULLY CONNECTED LAYER 2
+        # FULLY CONNECTED LAYER 2
         x = Dense(4096)(x)
         x = Activation('relu')(x)
 
-        #FULLY CONNECTED LAYER 3
-        x = Dense(1000)(x)
-        x =  Activation('relu')(x)
-
-        # OUTPUT LAYER
+        # FULLY CONNECTED LAYER 3
         output = self.output_layer(x, self.classes)
 
         model = Model(self.init, output, name='AlexNet2')
 
         return model
-
-    
-
-
-
-
-       
-    
--- alexnet/main.py
+++ alexnet/main.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
 from alexnet import AlexNet
-from alexnet_new import NewAlexNet
 sys.path.append("..") 
 from data_utils import *
 from datetime import datetime
@@ -19,64 +18,64 @@ IMAGESET_NAME = os.path.join(DATASET_PATH, 'fruits.pkl')
 
 if __name__ == "__main__":
     start = datetime.now()
-    x, y = build_image_dataset_from_dir(os.path.join(DATASET_PATH, 'fruits'),
-        dataset_file=IMAGESET_NAME,
-        resize=(100,100),
-        filetypes=['.png', '.jpg', '.JPG'],
-        convert_to_color=False,
-        shuffle_data=True,
-        categorical_Y=True)
+    # x, y = build_image_dataset_from_dir(os.path.join(DATASET_PATH, 'fruits'),
+    #     dataset_file=IMAGESET_NAME,
+    #     resize=(227,227),
+    #     filetypes=['.png', '.jpg', '.JPG'],
+    #     convert_to_color=False,
+    #     shuffle_data=True,
+    #     categorical_Y=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.30, 
-        random_state=1000)
-    sgd = optimizers.SGD(lr=0.01, momentum=0.9, decay=0.0005)
+    # X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.30, 
+    #     random_state=1000)
+    # sgd = optimizers.SGD(lr=0.01, momentum=0.9, decay=0.0005)
 
-    alexnet = AlexNet(input_shape=(100,100,3), classes=103)
+    alexnet = AlexNet(input_shape=(227,227,3), classes=2)
     model = alexnet.model()
-    # model.summary()  
-    if not (os.path.exists('../models/alexnet.h5')):
-        print("Training with {0}".format(len(X_train)))
-        print("Testing with {0}".format(len(X_test)))
+    model.summary()  
+    # if not (os.path.exists('../models/alexnet.h5')):
+    #     print("Training with {0}".format(len(X_train)))
+    #     print("Testing with {0}".format(len(X_test)))
 
-        # COMPILE MODEL
+    #     # COMPILE MODEL
        
-        model.compile(loss='categorical_crossentropy', optimizer=sgd,\
-        metrics=['accuracy'])
+    #     model.compile(loss='categorical_crossentropy', optimizer=sgd,\
+    #     metrics=['accuracy'])
 
-        #TRAIN MODEL
-        model.fit(X_train,y_train, batch_size=32, epochs=120, verbose=1, 
-           validation_split=0.2, shuffle=True) #callbacks=[HistoryCallback('../history/history.csv')])
+    #     #TRAIN MODEL
+    #     model.fit(X_train,y_train, batch_size=32, epochs=120, verbose=1, 
+    #        validation_split=0.2, shuffle=True) #callbacks=[HistoryCallback('../history/history.csv')])
         
-        score= model.evaluate(X_test, y_test, verbose=0)
-        print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
-        store_model(model, "../models/", "alexnet")
-        y_pred =  model.predict(X_test)
-        print(precision_recall_fscore_support(onehot_to_cat(y_test), onehot_to_cat(y_pred)))
-        labels = list(set(get_labels(y_test))) 
-        cm = confusion_matrix(get_labels(y_test),get_labels(y_pred))
-        plot_confusion_matrix(cm, labels)
-    else:
+    #     score= model.evaluate(X_test, y_test, verbose=0)
+    #     print("%s: %.2f%%" % (model.metrics_names[1], score[1]*227))
+    #     store_model(model, "../models/", "alexnet")
+    #     y_pred =  model.predict(X_test)
+    #     print(precision_recall_fscore_support(onehot_to_cat(y_test), onehot_to_cat(y_pred)))
+    #     labels = list(set(get_labels(y_test))) 
+    #     cm = confusion_matrix(get_labels(y_test),get_labels(y_pred))
+    #     plot_confusion_matrix(cm, labels)
+    # else:
 
-        labels = list(set(get_labels(y_test))) 
-        json_file = open('../models/alexnet.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close() 
-        loaded_model = model_from_json(loaded_model_json)
-        loaded_model.load_weights("../models/alexnet.h5")
+    #     labels = list(set(get_labels(y_test))) 
+    #     json_file = open('../models/alexnet.json', 'r')
+    #     loaded_model_json = json_file.read()
+    #     json_file.close() 
+    #     loaded_model = model_from_json(loaded_model_json)
+    #     loaded_model.load_weights("../models/alexnet.h5")
 
-        loaded_model.compile(loss='categorical_crossentropy', optimizer=sgd,
-         metrics=['acc'])
-        score = loaded_model.evaluate(X_test, y_test, verbose=0)
-        print(score)
-        import pdb; pdb.set_trace()
-        print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
-        #MODEL PREDICTION
-        prediction = loaded_model.predict(X_test)
-        fpr, tpr, thresholds = metrics.roc_curve(y_test,prediction)
-        auc = metrics.roc_auc_score(y_test,model.predict(X_test))
-        print(precision_recall_fscore_support(onehot_to_cat(y_test), onehot_to_cat(prediction), average='binary'))
-        cm = confusion_matrix(get_labels(y_test),get_labels(prediction))
-        plot_confusion_matrix(cm, labels, title="Confusion Matrix: AlexNet", cmap=plt.cm.Greens)
+    #     loaded_model.compile(loss='categorical_crossentropy', optimizer=sgd,
+    #      metrics=['acc'])
+    #     score = loaded_model.evaluate(X_test, y_test, verbose=0)
+    #     print(score)
+    #     import pdb; pdb.set_trace()
+    #     print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*227))
+    #     #MODEL PREDICTION
+    #     prediction = loaded_model.predict(X_test)
+    #     fpr, tpr, thresholds = metrics.roc_curve(y_test,prediction)
+    #     auc = metrics.roc_auc_score(y_test,model.predict(X_test))
+    #     print(precision_recall_fscore_support(onehot_to_cat(y_test), onehot_to_cat(prediction), average='binary'))
+    #     cm = confusion_matrix(get_labels(y_test),get_labels(prediction))
+    #     plot_confusion_matrix(cm, labels, title="Confusion Matrix: AlexNet", cmap=plt.cm.Greens)
 
-    time_elapsed = datetime.now() - start 
-    print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
+    # time_elapsed = datetime.now() - start 
+    # print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
