db2ea30ac68b9c4d8e0df11346a69c1d8e7c434c
pike-msonda <pike.msonda@gmail.com>
2019-08-04 22:33:16 +0300

    added new custom layers

--- alexnet/alexnet.py
+++ alexnet/alexnet.py
@@ -1,90 +1,86 @@
-"""
-    Author: Pike Msonda
-    Description: AlexNet implementation using Keras api
-"""
-
-from keras.layers import Input
-from keras.layers.merge import concatenate
-from keras.layers import Dense, Dropout, Flatten, Activation, Conv2D
-from keras.layers.convolutional import MaxPooling2D, AveragePooling2D, ZeroPadding2D
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model
-from keras.regularizers import l2
-
+import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Dropout, Reshape, Permute, Activation, Input, merge
+from custom_layers.spatial_pyramid_pooling import SpatialPyramidPooling
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras import backend as K
+from scipy.misc import imread, imresize, imsave
+from custom_layers.crosschannelnormalisation import crosschannelnormalization,splittensor
+K.set_image_dim_ordering('th')
 class AlexNet:
 
-    def __init__(self, input_shape, classes):
+    def __init__(self, input_shape, classes, weights_path=None):
         self.init = Input(input_shape)
         self.classes = classes
+        self.weights_path = weights_path
 
-    def conv_layer(self, x, filters,kernel_size, padding= "VALID", 
-            kernel_regularizer=l2(0), strides=(1,1), max_pooling=True, 
-            activation="relu", name=None): 
+    def model(self):  
 
-        x = Conv2D(filters, kernel_size, strides=strides, padding=padding, 
-            activation=activation)(x)
-        if (max_pooling):
-            x = MaxPooling2D(pool_size=(3,3), strides=(2,2))(x)
+        # COVOLUTIONAL LAYER 1  
+        x = Convolution2D(96, 11, 11,subsample=(4,4),activation='relu',
+                            name='conv_1')(self.init)
 
-        # x = BatchNormalization()(x) # apply batch normalisation. 
-        return x
+        x = MaxPooling2D((3, 3), strides=(2,2))(x)
+        x = crosschannelnormalization(name="convpool_1")(x) # normalisation instead of Batch Normalisation
+        x = ZeroPadding2D((2,2))(x)
 
-    def output_layer(self,x, classes):
-        x = Dense(units=classes)(x)
-        x = Activation('softmax')(x)
-        return x
-    
-    def dense_layer(self,x,units):
-        x = Dense(units)(x)
-        x = Activation('relu')(x)
-        x = Dropout(0.5)(x)
-        
-        return x
+        # COVOLUTIONAL LAYER 2  
+        x = merge([
+            Convolution2D(128,5,5,activation="relu",name='conv_2_'+str(i+1))(
 
-    def model(self):    
-        # 1st LAYER
-        x =  self.conv_layer(self.init, filters=96, kernel_size=(11,11), strides=(4,4),
-            padding="valid", max_pooling=True, activation='relu', name='conv_1')
+                splittensor(ratio_split=2,id_split=i)(x)
 
-        x = BatchNormalization()(x) # apply batch normalisation.
-        # 2nd Layer
-        x =  self.conv_layer(x, filters=256, kernel_size=(5,5),strides=(1,1),
-            padding="VALID", max_pooling=True, name="conv_2")
-        
-        x = BatchNormalization()(x) # apply batch normalisation.
-        
-        # 3RD LAYER
-        x =  self.conv_layer(x, filters=384, kernel_size=(3,3),strides=(1,1),
-            padding="VALID",max_pooling=False, name="conv_3")
-        
+            ) for i in range(2)], mode='concat',concat_axis=1,name="conv_2")
 
-        # 4Th LAYER
-        x =  self.conv_layer(x, filters=384, kernel_size=(3,3),strides=(1,1), 
-            padding="VALID", max_pooling=False, name="conv_4")
+        x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+        x = crosschannelnormalization()(x)
+        x = ZeroPadding2D((1,1))(x)
 
-        # 5Th LAYER
-        x =  self.conv_layer(x, filters=256, kernel_size=(3,3),strides=(1,1),
-            padding="VALID", max_pooling=True, name="conv_5")
+        # COVOLUTIONAL LAYER 3 
+        x = Convolution2D(384,3,3,activation='relu',name='conv_3')(x)
 
-        # 6 FLATTEN 
-        x = Flatten()(x)
+        x = ZeroPadding2D((1,1))(x)
+        x = merge([
+            Convolution2D(192,3,3,activation="relu",name='conv_4_'+str(i+1))(
+
+                splittensor(ratio_split=2,id_split=i)(x)
+
+            ) for i in range(2)], mode='concat',concat_axis=1,name="conv_4")
+        x = ZeroPadding2D((1,1))(x)
 
 
-        # Fully Connected LAYER 1
-        x = Dense(4096)(x)
-        x = Activation('relu')(x)
+        # COVOLUTIONAL LAYER 5 
+        x = merge([
+            Convolution2D(128,3,3,activation="relu",name='conv_5_'+str(i+1))(
+
+                splittensor(ratio_split=2,id_split=i)(x)
+            ) for i in range(2)], mode='concat',concat_axis=1,name="conv_5")
+
+        x = MaxPooling2D((3, 3), strides=(2,2),name="convpool_5")(x)
+
+
+        # Flatten Tensor
+        x = Flatten(name="flatten")(x)
+
+        # FUlly connected layer 1
+        x = Dense(4096, activation='relu',name='dense_1')(x)
         x = Dropout(0.5)(x)
 
-        # FULLY CONNECTED LAYER 2
-        x = Dense(4096)(x)
-        x = Activation('relu')(x)
+        # FUlly connected layer 2
+        x = Dense(4096, activation='relu',name='dense_2')(x)
         x = Dropout(0.5)(x)
+        
 
-        x = Dense(1000)(x)
-        x = Activation('relu')(x)
+        # OUTPUT Layer
+        x = Dense(self.classes,name='dense_3')(x)
+        ouput  = Activation("softmax",name="softmax")(x)
 
-        output = self.output_layer(x, self.classes)
 
-        model = Model(self.init, output, name='AlexNet2')
+        model = Model(input=self.init, output=ouput)
+
+        if self.weights_path:
+            model.load_weights(self.weights_path)
 
         return model
+
--- alexnet/main.py
+++ alexnet/main.py
@@ -8,19 +8,18 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import confusion_matrix
-from callbacks.history_callback import HistoryCallback
 from keras.models import load_model
 from keras import optimizers
 from sklearn import metrics
 # from keras.backend import manual_variable_initialization(True)
 DATASET_PATH = '../data/train/'
 TEST_PATH = 'D:\Data/test/'
-TEST_PATH_NAME=os.path.join(TEST_PATH, 'all.pkl')
-IMAGESET_NAME = os.path.join(DATASET_PATH, 'turkey.pkl')
+TEST_PATH_NAME=os.path.join(TEST_PATH, 'china.pkl')
+IMAGESET_NAME = os.path.join(DATASET_PATH, 'china.pkl')
 
 if __name__ == "__main__":
     start = datetime.now()
-    trainX, trainY = build_image_dataset_from_dir(os.path.join(DATASET_PATH, 'turkey'),
+    trainX, trainY = build_image_dataset_from_dir(os.path.join(DATASET_PATH, 'china'),
         dataset_file=IMAGESET_NAME,
         resize=None,
         filetypes=['.png'],
@@ -28,54 +27,52 @@ if __name__ == "__main__":
         shuffle_data=True,
         categorical_Y=True)
 
-    testX, testY = build_image_dataset_from_dir(os.path.join(TEST_PATH, 'all'),
+    testX, testY = build_image_dataset_from_dir(os.path.join(TEST_PATH, 'china'),
         dataset_file=TEST_PATH_NAME,
         resize=(227, 227),
         filetypes=['.png'],
         convert_to_color=True,
         shuffle_data=True,
         categorical_Y=True)
-    # import pdb; pdb.set_trace()
+
     X_train, X_test, y_train, y_test = train_test_split(trainX,trainY, test_size=0.30, 
         random_state=1000)
     sgd = optimizers.SGD(lr=0.01, momentum=0.9, decay=0.0005,nesterov=False)
 
-    alexnet = AlexNet(input_shape=(227,227,3), classes=2)
+    alexnet = AlexNet(input_shape=(3,227,227), classes=2)
     model = alexnet.model()
-    # model.summary()  
+    model.summary()  
     if not (os.path.exists('../models/alexnet.h5')):
         print("Training with {0}".format(len(trainX)))
         print("Testing with {0}".format(len(testX)))
 
         # COMPILE MODEL
        
-        model.compile(loss='categorical_crossentropy', optimizer=sgd,\
-        metrics=['accuracy'])
+        model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
         #TRAIN MODEL
-        model.fit(X_train,y_train, batch_size=32, epochs=80, verbose=1, 
-           validation_data=(X_test, y_test), shuffle=True) #callbacks=[HistoryCallback('../history/history.csv')])
+        model.fit(np.moveaxis(X_train, -1, 1) ,y_train, batch_size=32, nb_epoch=10, verbose=1, 
+           validation_data=(np.moveaxis(X_test,-1,1), y_test), shuffle=True)
         
-        score= model.evaluate(testX, testY, verbose=0)
+        score= model.evaluate(np.moveaxis(testX, -1, 1), testY, verbose=0)
         print("%s: %.2f%%" % (model.metrics_names[1], score[1]))
         store_model(model, "../models/", "alexnet")
-        y_pred =  model.predict(testX)
-        print(precision_recall_fscore_support(onehot_to_cat(testY), onehot_to_cat(y_pred)))
+        y_pred =  model.predict(np.moveaxis(testX, -1, 1))
+        print(precision_recall_fscore_support(onehot_to_cat(np.moveaxis(testX, -1, 1)), onehot_to_cat(y_pred)))
         labels = list(set(get_labels(testY))) 
-        cm = confusion_matrix(get_labels(testY),get_labels(y_pred))
+        cm = confusion_matrix(get_labels(np.moveaxis(testX, -1, 1)),get_labels(y_pred))
         plot_confusion_matrix(cm, labels)
     else:
-
         labels = list(set(get_labels(testY))) 
 
         loaded_model = load_model("../models/alexnet.h5")
 
+        import pdb; pdb.set_trace()
         loaded_model.compile(loss='categorical_crossentropy', optimizer=sgd,
          metrics=['acc'])
-        score = loaded_model.evaluate(testX, testY, verbose=1)
+        score = loaded_model.evaluate(np.moveaxis(testX, -1, 1), testY, verbose=1)
         print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]))
-        y_pred =  loaded_model.predict(testX)
-        # import pdb; pdb.set_trace()
+        y_pred =  loaded_model.predict(np.moveaxis(testX, -1, 1))
         # print(precision_recall_fscore_support(onehot_to_cat(testY), onehot_to_cat(y_pred)))
         labels = list(set(get_labels(testY))) 
         cm = confusion_matrix(get_labels(testY),get_labels(y_pred))
